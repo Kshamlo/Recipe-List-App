7abfab5963b673d8f6d7051724047b888c1eb238
Kora Shamloo <shamlookora866@gmail.com>
2021-08-30 16:49:42 -0400

    Initial Commit

---
+++ Recipe List App/Recipe_List_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe_List_AppApp.swift
+//  Recipe List App
+//
+//  Created by Kora Shamloo on 8/30/21.
+//
+
+import SwiftUI
+
+@main
+struct Recipe_List_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
